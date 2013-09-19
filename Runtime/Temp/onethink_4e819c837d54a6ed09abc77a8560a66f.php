ad6a5249478191af4f9e9a2046adb0d95b2db168
xcation <1063442377@qq.com>
2013-09-19 22:46:08 +0800

    Revert "pull"
    This reverts commit 6d726c06a22f51d028fd3df5df6f29b8498e4515.

--- Runtime/Temp/onethink_4e819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:10:{s:10:"pageHeader";a:1:{i:0;s:0:"";}s:10:"pageFooter";a:1:{i:0;s:0:"";}s:16:"documentEditForm";a:1:{i:0;s:0:"";}s:19:"documentDetailAfter";a:1:{i:0;s:0:"";}s:20:"documentDetailBefore";a:1:{i:0;s:0:"";}s:20:"documentSaveComplete";a:1:{i:0;s:0:"";}s:23:"documentEditFormContent";a:1:{i:0;s:0:"";}s:16:"adminArticleEdit";a:1:{i:0;s:14:"EditorForAdmin";}s:10:"AdminIndex";a:3:{i:0;s:8:"SiteStat";i:1;s:7:"DevTeam";i:2;s:10:"SystemInfo";}s:12:"topicComment";a:1:{i:0;s:0:"";}}
-?>
